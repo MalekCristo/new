d0b1895bffa7ef9190dd1f3a1321e4f387c9dec9
MalekCristo <malekcristo4@gmail.com>
2024-09-25 22:56:02 +0100

    third commit

--- script.js
+++ script.js
@@ -20,3 +20,12 @@ function opentab(tabname){
 
 
 }
+
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
